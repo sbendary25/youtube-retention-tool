713c7064939fd3cf871316680def796753718fab
Samer <sbendary25@gmail.com>
2022-03-17 16:15:47 -0700

    add scopes

--- src/App.js
+++ src/App.js
@@ -37,7 +37,12 @@ function App() {
       .signIn({
         scopes: [
           "https://www.googleapis.com/auth/youtube.readonly",
-          "https://www.googleapis.com/auth/youtube.force-ssl"
+          "https://www.googleapis.com/auth/youtube.force-ssl",
+          "https://www.googleapis.com/auth/yt-analytics.readonly",
+          "https://www.googleapis.com/auth/yt-analytics-monetary.readonly",
+          "https://www.googleapis.com/auth/youtube.readonly",
+          "https://www.googleapis.com/auth/youtube",
+          "https://www.googleapis.com/auth/youtubepartner"
         ]
       })
       .then(
